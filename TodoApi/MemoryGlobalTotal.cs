dc08db9d6f4b2d12c21d4cc88266367a11af65fc
uvivi161 <uvi69161@gmail.com>
2025-02-06 21:48:00 +0200

    good server

--- TodoApi/MemoryGlobalTotal.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TodoApi;
-
-public partial class MemoryGlobalTotal
-{
-    public string? TotalAllocated { get; set; }
-}
--- TodoApi/XMemoryGlobalTotal.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TodoApi;
-
-public partial class XMemoryGlobalTotal
-{
-    public decimal? TotalAllocated { get; set; }
-}
